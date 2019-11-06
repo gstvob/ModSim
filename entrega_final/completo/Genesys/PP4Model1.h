284d5adecd3ddd3d8f127504bced24180691972d
gstvob <gustavo.borgesfr@gmail.com>
2019-11-06 03:03:11 -0300

    Getting ready for the final deliver

---
+++ entrega_final/completo/Genesys/PP4Model1.h
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   PP4Model1.h
+ * Author: gstvob
+ *
+ * Created on 24 de Setembro de 2019, 16:43
+ */
+
+#ifndef PP4MODEL1_H
+#define PP4MODEL1_H
+
+#include "BaseConsoleGenesysApplication.h"
+
+class PP4Model1 : public BaseConsoleGenesysApplication{
+public:
+    PP4Model1();
+public:
+    virtual int main(int argc, char** argv);
+};
+#endif /* PP4Model1 */
+
---
+++ entrega_final/completo/Genesys/TestMore.h
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   TestMore.h
+ * Author: gstvob
+ *
+ * Created on 24 de Setembro de 2019, 16:43
+ */
+
+#ifndef TESTMORE_H
+#define TESTMORE_H
+
+#include "BaseConsoleGenesysApplication.h"
+
+class TestMore : public BaseConsoleGenesysApplication{
+public:
+    TestMore();
+public:
+    virtual int main(int argc, char** argv);
+};
+#endif /* TestMore */
+
